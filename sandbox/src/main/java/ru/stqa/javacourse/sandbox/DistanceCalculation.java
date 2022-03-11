e050b6cdf66ae4c3e70af970dc26b3d21ed1cf20
ArtemLogozovski <artemlog@mail.ru>
2022-03-12 01:20:07 +0700

    Добавлен тест для вычисления площади квадрата

--- sandbox/src/main/java/ru/stqa/javacourse/sandbox/DistanceCalculation.java
+++ sandbox/src/main/java/ru/stqa/javacourse/sandbox/DistanceCalculation.java
@@ -4,7 +4,7 @@ public class DistanceCalculation {
 
   public static void main(String[] args) {
     Point p1 = new Point(2, 2);
-    Point p2 = new Point(4, 4);
+    Point p2 = new Point(5, 0);
     System.out.println("Расстояние между точкой (" + p1.x + "; " + p1.y + ") и точкой (" + p2.x + "; " + p2.y + ") = " + p1.distance(p2));
   }
 }
