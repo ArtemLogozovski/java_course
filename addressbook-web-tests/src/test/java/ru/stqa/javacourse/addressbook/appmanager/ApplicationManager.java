e0aaaeeb410614c92a970cf9b6f0b32f76f0289f
ArtemLogozovski <artemlog@mail.ru>
2022-04-01 00:07:20 +0700

    Добавлена возможность запускать тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/appmanager/ApplicationManager.java
@@ -1,11 +1,16 @@
 package ru.stqa.javacourse.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+
+  private String browser;
   WebDriver wd;
 
   private ContactHelper contactHelper;
@@ -13,8 +18,18 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     navigationHelper = new NavigationHelper(wd);
