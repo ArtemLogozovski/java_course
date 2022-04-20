d8b636f8cce668abd0045d2e082f5d8d9e836658
ArtemLogozovski <artemlog@mail.ru>
2022-04-21 00:46:24 +0700

    Добавлены лямбда-выражения для тестов создания группы и контакта

--- addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javacourse/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.javacourse.addressbook.model.ContactData;
 import ru.stqa.javacourse.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,14 +22,8 @@ public class ContactCreationTests extends TestBase {
     app.getContactHelper().createContact(contact);
     List <ContactData> after = app.getContactHelper().getContactsList();
     Assert.assertEquals(after.size(), before.size() + 1);
-
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
